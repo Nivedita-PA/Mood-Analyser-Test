871aab7a81576091ce09e06be06b4fba93a74b40
Nivedita-PA <npal4661@gmail.com>
2021-06-07 00:34:03 +0530

    test case introduced for returning happy mood

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -9,4 +9,10 @@ public class MoodAnalyserTest {
         String mood = moodAnalyser.analyseMood("This is a sad message");
         Assert.assertEquals("SAD", mood);
     }
+    @Test
+    public void givenMood_whenNotSad_ShouldReturnHappy(){
+        MoodAnalyser moodAnalyserT = new MoodAnalyser();
+        String mood = moodAnalyserT.analyseMood("This is a happy message");
+        Assert.assertEquals("HAPPY", mood);
+    }
 }
