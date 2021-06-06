02af990456c5441640d43d1d43d6d005bc835d29
Nivedita-PA <npal4661@gmail.com>
2021-06-07 00:40:20 +0530

    Code refactored for taking messages in constructor

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,6 +1,18 @@
 public class MoodAnalyser {
 
+    private String message;
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
     public String analyseMood(String message) {
+        this.message = message;
+        return message;
+    }
+
+
+    public String analyseMood() {
         if (message.contains("sad")) {
             return "SAD";
         } else {
